d7ed2c95e3def5c2b342e775552ebb06afeded5d
mmhajizadeh <mmhajizadeh1384@gmail.com>
2026-07-04 00:05:47 +0330

    Add repositories for Advertisement, City and Category

---
+++ backend/src/main/java/com/secondhand/backend/repository/AdvertisementRepository.java
@@ -0,0 +1,10 @@
+package com.secondhand.backend.repository;
+
+import com.secondhand.backend.model.Advertisement;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface AdvertisementRepository extends JpaRepository<Advertisement, Long> {
+
+}
