6227a8023e92fc058189a6d6b8e2636846bd69b5
mmhajizadeh <mmhajizadeh1384@gmail.com>
2026-07-11 20:07:02 +0330

    Refactor MainController and UI to use responsive FlowPane for dynamic ad card loading

--- frontend/src/main/java/com/secondhand/frontend/MainController.java
+++ frontend/src/main/java/com/secondhand/frontend/MainController.java
@@ -1,4 +1,44 @@
 package com.secondhand.frontend;
 
-public class MainController {
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.layout.FlowPane;
+import javafx.scene.layout.HBox;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class MainController implements Initializable {
+    @FXML
+    private FlowPane adsContainer;
+
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        loadAds();
+    }
+
+    private void loadAds() {
+        for (int i = 1; i <= 6; i++) {
+            try {
+                FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("ad-card.fxml"));
+                HBox cardBox = fxmlLoader.load();
+
+                AdCardController cardController = fxmlLoader.getController();
+                cardController.setAdData(
+                        "دوچرخه کوهستانی مدل " + i,
+                        "5,000,000 تومان",
+                        "تهران | دقایقی پیش",
+                        "dummy_image_url"
+                );
+
+                adsContainer.getChildren().add(cardBox);
+
+            } catch (IOException e) {
+                System.err.println("Error loading ad card: " + e.getMessage());
+                e.printStackTrace();
+            }
+        }
+    }
 }
